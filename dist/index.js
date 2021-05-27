4e27448a2fedc20a2ccc879e145502a2a7602c5c
Randy Lough <randallmlough@gmail.com>
2021-05-27 15:23:35 -0700

    transpiling and ie11 support

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,97 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = exports.supportedBrowsers = void 0;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-require("core-js/modules/es6.regexp.match.js");
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-require("core-js/modules/es6.regexp.constructor.js");
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-require("core-js/modules/es6.object.keys.js");
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var userAgent = function userAgent() {
-  return (navigator && navigator.userAgent || "").toLowerCase();
-};
+var IsSupported = /*#__PURE__*/function () {
+  function IsSupported() {
+    var supportedBrowsers = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
-var vendor = function vendor() {
-  return (navigator && navigator.vendor || "").toLowerCase();
-};
+    _classCallCheck(this, IsSupported);
+
+    _defineProperty(this, "userAgent", function () {
+      return (navigator && navigator.userAgent || "").toLowerCase();
+    });
+
+    _defineProperty(this, "vendor", function () {
+      return (navigator && navigator.vendor || "").toLowerCase();
+    });
+
+    this.supportedBrowsers = supportedBrowsers;
+    this.browsersRegex = new RegExp(Object.keys(supportedBrowsers).join("|"));
+  }
+
+  _createClass(IsSupported, [{
+    key: "isSupported",
+    value: function isSupported() {
+      var browser = this.getBrowser();
+      var version = this.supportedBrowsers[browser];
+      if (!version || version == "none") return false;
+      if (version == "any") return true;
+
+      switch (browser) {
+        case "msie":
+        case "trident":
+          return this.isIe(version);
+
+        case "edge":
+          return this.isEdge(version);
+
+        case "firefox":
+          return this.isFirefox(version);
+
+        case "chrome":
+          return this.isChrome(version);
+      }
+    }
+  }, {
+    key: "compareVersion",
+    value: function compareVersion(version, range) {
+      var string = range + "";
+      var n = +(string.match(/\d+/) || NaN);
+      var op = string.match(/^[<>]=?|/)[0];
+      return comparator[op] ? comparator[op](version, n) : version == n || n !== n;
+    }
+  }, {
+    key: "getBrowser",
+    value: function getBrowser() {
+      var match = this.userAgent().match(this.browsersRegex);
+      if (!match) return null;
+      return match[0];
+    }
+  }, {
+    key: "isChrome",
+    value: function isChrome(range) {
+      var match = /google inc/.test(this.vendor()) ? this.userAgent().match(/(?:chrome|crios)\/(\d+)/) : null;
+      return match !== null && this.compareVersion(match[1], range);
+    }
+  }, {
+    key: "isIe",
+    value: function isIe(range) {
+      var match = this.userAgent().match(/(?:msie |trident.+?; rv:)(\d+)/);
+      return match !== null && this.compareVersion(match[1], range);
+    }
+  }, {
+    key: "isEdge",
+    value: function isEdge(range) {
+      var match = this.userAgent().match(/edge\/(\d+)/);
+      return match !== null && this.compareVersion(match[1], range);
+    }
+  }, {
+    key: "isFirefox",
+    value: function isFirefox(range) {
+      var match = this.userAgent().match(/(?:firefox|fxios)\/(\d+)/);
+      return match !== null && this.compareVersion(match[1], range);
+    }
+  }]);
+
+  return IsSupported;
+}();
 
 var comparator = {
   "<": function _(a, b) {
@@ -33,87 +107,3 @@ var comparator = {
     return a >= b;
   }
 };
-
-function compareVersion(version, range) {
-  var string = range + "";
-  var n = +(string.match(/\d+/) || NaN);
-  var op = string.match(/^[<>]=?|/)[0];
-  return comparator[op] ? comparator[op](version, n) : version == n || n !== n;
-}
-
-var supportedBrowsers = {
-  msie: "none",
-  //ie / internet explorer
-  trident: "none",
-  //ie / internet explorer
-  edge: ">1",
-  // example version range for now
-  chrome: "any",
-  firefox: "any",
-  opera: "none"
-};
-exports.supportedBrowsers = supportedBrowsers;
-
-function isSupported() {
-  var browsers = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : supportedBrowsers;
-  var browser = getBrowser();
-  var version = browsers[browser];
-  if (!version || version == "none") return false;
-  if (version == "any") return true;
-
-  switch (browser) {
-    case "msie":
-    case "trident":
-      return isIe(version);
-
-    case "edge":
-      return isEdge(version);
-
-    case "firefox":
-      return isFirefox(version);
-
-    case "chrome":
-      return isChrome(version);
-  }
-}
-
-var browsersRegex = new RegExp(Object.keys(supportedBrowsers).join("|"));
-
-function getBrowser() {
-  var match = userAgent().match(browsersRegex);
-  if (!match) return null;
-  return match[0];
-} // can take a number or a string. Can also take a string prefixed with a comparable like, > or >=
-// example, chrome('>=90')
-
-
-function isChrome(range) {
-  var match = /google inc/.test(vendor()) ? userAgent().match(/(?:chrome|crios)\/(\d+)/) : null;
-  return match !== null && compareVersion(match[1], range);
-}
-
-function isIe(range) {
-  var match = userAgent().match(/(?:msie |trident.+?; rv:)(\d+)/);
-  return match !== null && compareVersion(match[1], range);
-}
-
-function isEdge(range) {
-  var match = userAgent().match(/edge\/(\d+)/);
-  return match !== null && compareVersion(match[1], range);
-}
-
-function isFirefox(range) {
-  var match = userAgent().match(/(?:firefox|fxios)\/(\d+)/);
-  return match !== null && compareVersion(match[1], range);
-}
-
-var _default = {
-  supportedBrowsers: supportedBrowsers,
-  compareVersion: compareVersion,
-  isSupported: isSupported,
-  isChrome: isChrome,
-  isIe: isIe,
-  isEdge: isEdge,
-  isFirefox: isFirefox
-};
-exports.default = _default;
